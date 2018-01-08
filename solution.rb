131a7e889b48736995e70eb87c7b5c892a9c5cbc
anderson <anderson@debian.anderson.com>
2018-01-08 18:49:46 -0500

    Ajuste 2 a Salúdame 1

--- solution.rb
+++ solution.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 
-get '/' do
-"<h1>Hola desconocido</h1>"
-end
 
-get '/:nombre' do
-  "<h1>Hola #{params[:nombre]}</h1>"
+get '/' do
+	unless params[:nombre]
+  		"<h1>Hola desconocido!</h1>"
+	else
+  		"<h1>Hola #{params[:nombre]}!</h1>"
+	end
 end
